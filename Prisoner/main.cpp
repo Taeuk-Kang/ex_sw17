6fccbf1d803093e869eee19b209c3b12d896b87e
Taewook Kang <xodnr6334@tvstorm.com>
2019-10-01 20:24:22 +0900

    add function at prisoner

--- Prisoner/main.cpp
+++ Prisoner/main.cpp
@@ -2,14 +2,20 @@
 
 using namespace std;
 int L, N, M, R, C;
+int Map[50*50];
+int Res[50*50];
 int serchPrisoner()
 {
+    int val = Map[M*R+C];
+    switch (val);
+    {
+	case 
 
+    }
 }
 int main()
 {
     int numTest, result;
-    int Map[50*50];
     scanf("%d",numTest);
 
     for (int curTest = 0; curTest<numTest; curTest++)
@@ -19,11 +25,19 @@ int main()
         scanf("%d", &L);
         scanf("%d", &C);
         scanf("%d", &L);
-
+	for (int input = 0; input < N*M; input ++)
+	{
+	    scanf("%d", &Map[input]);
+	}
 
         result = serchPrisoner();
 
         printf("#%d %d", curTest, result);
+	for (int reset = 0; reset < N*M; reset++)
+	{
+	    Map[reset] = 0;
+	    Res[reset] = 0;
+	}
     }
     return 0;
 }
